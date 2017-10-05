75a8574587f7491ca08206f8fe698ac96519ce84
moldovaa <andrei.moldovan@msg-systems.com>
2017-10-05 14:58:54 +0300

    Added spring boot application tests

---
+++ InMemoryAuthorizationService/src/test/java/com/andrei/security/authorization/AuthorizationServerAppTest.java
@@ -0,0 +1,25 @@
+package com.andrei.security.authorization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *  Tests for the SpringBootApplication
+ *
+ * @author Andrei Moldovan
+ * @since 05.10.2017
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthorizationServerAppTest {
+		/**
+		 * Test for loading the context.
+		 */
+		@Test
+		public void contextLoads() throws Exception {
+			// nothing to do
+		}
+}
+
